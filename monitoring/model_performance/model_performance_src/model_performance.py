26185ca701c688f26862d8955ac5a2bf7323b6c6
Martin Peck <mpeck@microsoft.com>
2023-04-04 12:16:15 +0000

    fix formatting

--- monitoring/model_performance/model_performance_src/model_performance.py
+++ monitoring/model_performance/model_performance_src/model_performance.py
@@ -2,12 +2,13 @@ import argparse
 import mlflow
 import os
 import pandas as pd
-from sklearn.compose import  make_column_selector as selector
+from sklearn.compose import make_column_selector as selector
 import logging
 from opencensus.ext.azure.log_exporter import AzureLogHandler
-from sklearn.metrics import accuracy_score,f1_score,precision_score,recall_score,roc_auc_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
 
-def read_data(inference_data_path,groundtruth_data_path,index_name):
+
+def read_data(inference_data_path, groundtruth_data_path, index_name):
     '''
     In the template, the assumption is that the data are stored in csv files,
     Change this function to read the data using the appropriate method for your data type.
@@ -15,9 +16,10 @@ def read_data(inference_data_path,groundtruth_data_path,index_name):
     '''
     inf_df = pd.read_csv(inference_data_path)
     ground_df = pd.read_csv(groundtruth_data_path)
-    df = pd.merge(inf_df,ground_df,on=index_name,how='outer')
-    
+    df = pd.merge(inf_df, ground_df, on=index_name, how='outer')
+
     return df
+
 
 def get_metrics(df):
     '''
@@ -26,16 +28,18 @@ def get_metrics(df):
     output: metrics_dict: a dictionary containing the metrics
     '''
     # Modify this part to include the metrics you would like to monitor
-    metrics_dict = metrics = {'accuracy':accuracy_score(df['ground_truth'],df['pred']),
-               'f1_score':f1_score(df['ground_truth'],df['pred']),
-               'precision':precision_score(df['ground_truth'],df['pred']),
-               'recall':recall_score(df['ground_truth'],df['pred']),
-               'roc':roc_auc_score(df['ground_truth'],df['pred']),}
+    metrics_dict = metrics = {'accuracy': accuracy_score(df['ground_truth'], df['pred']),
+                              'f1_score': f1_score(df['ground_truth'], df['pred']),
+                              'precision': precision_score(df['ground_truth'], df['pred']),
+                              'recall': recall_score(df['ground_truth'], df['pred']),
+                              'roc': roc_auc_score(df['ground_truth'], df['pred']), }
     return metrics_dict
+
 
 def main(args):
     logger = logging.getLogger(__name__)
-    logger.addHandler(AzureLogHandler(connection_string=args.logger_connection_string))
+    logger.addHandler(AzureLogHandler(
+        connection_string=args.logger_connection_string))
 
     mlflow.set_tracking_uri(args.mlflow_uri)
     mlflow.start_run()
@@ -43,10 +47,10 @@ def main(args):
 
     inference_data_path = args.inference_data_path
     groundtruth_data_path = args.groundtruth_data_path
-    
-    df = read_data(inference_data_path,groundtruth_data_path,args.index_name)
+
+    df = read_data(inference_data_path, groundtruth_data_path, args.index_name)
     metrics = get_metrics(df)
-    
+
     mlflow.log_metrics(metrics)
     metrics['run_id'] = run_id
 
@@ -56,13 +60,13 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str,required=True)
-    parser.add_argument('--inference_data_path', type=str,required=True)
-    parser.add_argument('--groundtruth_data_path', type=str,required=True)
-    parser.add_argument('--index_name', type=str,required=True)
-    parser.add_argument('--mlflow_uri', type=str,required=True)
-    parser.add_argument('--logger_connection_string', type=str,required=True)
-    parser.add_argument('--model_version', type=str,required=False)
+    parser.add_argument('--model_name', type=str, required=True)
+    parser.add_argument('--inference_data_path', type=str, required=True)
+    parser.add_argument('--groundtruth_data_path', type=str, required=True)
+    parser.add_argument('--index_name', type=str, required=True)
+    parser.add_argument('--mlflow_uri', type=str, required=True)
+    parser.add_argument('--logger_connection_string', type=str, required=True)
+    parser.add_argument('--model_version', type=str, required=False)
 
     args = parser.parse_args()
 

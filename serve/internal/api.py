aca6e89effa7f2616a35a93f925e7e496f17a0cd
Martin Peck <mpeck@microsoft.com>
2023-04-06 15:25:24 +0000

    add logging to api

--- serve/internal/api.py
+++ serve/internal/api.py
@@ -13,18 +13,50 @@
 #  limitations under the License.
 
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.trace.span import SpanKind
+from opencensus.trace.attributes_helper import COMMON_ATTRIBUTES
+
 from serve import entrypoint
 from .about import generate_about_json
-from .azure_logging import initialize_logging
+from .azure_logging import initialize_logging, disable_unwanted_loggers
+
+
+logger = logging.getLogger(__name__)
+
+HTTP_URL = COMMON_ATTRIBUTES['HTTP_URL']
+HTTP_STATUS_CODE = COMMON_ATTRIBUTES['HTTP_STATUS_CODE']
 
 # create fastapi app
 app = FastAPI()
 
-
 @app.on_event("startup")
 async def initialize_logging_on_startup():
-    initialize_logging(logging.DEBUG)
+    initialize_logging(logging.INFO)
+    disable_unwanted_loggers()
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next) -> Response:
+    tracer = Tracer(exporter=AzureExporter(sampler=ProbabilitySampler(1.0)))
+    with tracer.span("main") as span:
+        span.span_kind = SpanKind.SERVER
+            
+        response = await call_next(request)
+
+        tracer.add_attribute_to_current_span(
+                attribute_key=HTTP_STATUS_CODE,
+                attribute_value=response.status_code)
+        tracer.add_attribute_to_current_span(
+            attribute_key=HTTP_URL,
+            attribute_value=str(request.url))
+        
+    return response
+
 
 @app.get("/")
 def root():
